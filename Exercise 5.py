a27d79cb7735625d68aee3c2c6ed5cef5c6a9da3
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2020-07-02 15:14:16 +0530

    Initial commit

--- Exercise 5.py
+++ Exercise 5.py
@@ -16,5 +16,5 @@ b=list(map(int,list((input("Enter list two elements separated by spaces : ").spl
 #a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-li=list(set(filter(lambda x:x in b,a)))
+li=list(set([i for i in a if i in b]))
 print(li)
